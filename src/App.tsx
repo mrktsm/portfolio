c4c965d55e4a9dbd8baf2b29e4bba563993c413f
Marko Tsymbaluk <markotsymbaluk@gmail.com>
2025-01-27 17:03:59 -0500

    Add Font

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <span>Hi, I'm Marko!</span>
+      <span style={{ fontFamily: "Hooey-Regular" }} className="text-9xl">
+        Hi, I'm Marko!
+      </span>
     </>
   );
 }
