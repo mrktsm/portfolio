4b084e3bc34671a65779c7c0f29f56010ae77676
Marko Tsymbaluk <markotsymbaluk@gmail.com>
2025-01-29 12:27:03 -0500

    Add Technologies

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
 import { useEffect, useRef, useState } from "react";
 import { BsArrowDown } from "react-icons/bs";
 import { MagicWord } from "./components/MagicWord.js";
+import { FaReact } from "react-icons/fa";
+import { SiSpringboot } from "react-icons/si";
+import { PiOpenAiLogoLight } from "react-icons/pi";
+import { SiGooglecloud } from "react-icons/si";
+import { SiFastapi } from "react-icons/si";
+import { SiTensorflow } from "react-icons/si";
+import { FaAws } from "react-icons/fa";
+import { SiOllama } from "react-icons/si";
+import { IoLogoElectron } from "react-icons/io5";
+import { GrMysql } from "react-icons/gr";
+import { FaRaspberryPi } from "react-icons/fa";
+import { BiLogoPostgresql } from "react-icons/bi";
 
 const App = () => {
   const [isVisible, setIsVisible] = useState(false);
@@ -8,37 +20,53 @@ const App = () => {
 
   const projects = [
     {
-      title: "CodeCafe",
-      imageUrl: "public/images/codecafe2.png",
-      description: "A full-stack web application with real-time features.",
-      technologies: ["React", "Spring Boot", "PostgreSQL"],
-      link: "https://github.com/mrktsm/spamurai",
-    },
-    {
       title: "Spamurai",
+      subtitle: "AI Email Companion",
       imageUrl: "public/images/spamurai_white_2.png",
       description: "A full-stack web application with real-time features.",
-      technologies: ["React", "Spring Boot", "PostgreSQL"],
+      technologies: [
+        <FaReact />,
+        <SiFastapi />,
+        <BiLogoPostgresql />,
+        <SiTensorflow />,
+        <FaAws />,
+      ],
       link: "https://github.com/mrktsm/spamurai",
     },
     {
       title: "Dermafyr",
+      subtitle: "YCP '24 Winner",
       description: "A full-stack web application with real-time features.",
-      technologies: ["React", "Spring Boot", "PostgreSQL"],
+      technologies: [
+        <FaReact />,
+        <IoLogoElectron />,
+        <SiFastapi />,
+        <SiTensorflow />,
+        <SiOllama />,
+        <GrMysql />,
+        <FaRaspberryPi />,
+      ],
       link: "https://github.com/mrktsm/spamurai",
     },
     {
       title: "Climately",
+      subtitle: "HackHarvard '24",
       imageUrl: "public/images/climately-final.png",
       description: "A full-stack web application with real-time features.",
-      technologies: ["React", "Spring Boot", "PostgreSQL"],
+      technologies: [
+        <FaReact />,
+        <SiSpringboot />,
+        <SiGooglecloud />,
+        <PiOpenAiLogoLight />,
+      ],
       link: "https://github.com/mrktsm/spamurai",
     },
     {
       title: "Thesaurus Rex",
-      imageUrl: "public/images/trexpng-cover.png",
+      subtitle: "Dictionary at a Click",
+      imageUrl: "public/images/trexpng-cover-2.png",
       description: "A full-stack web application with real-time features.",
-      technologies: ["React", "Spring Boot", "PostgreSQL"],
+      technologies: [<FaReact />, <SiSpringboot />, <PiOpenAiLogoLight />],
       link: "https://github.com/mrktsm/spamurai",
     },
   ];
@@ -122,7 +150,7 @@ const App = () => {
       <div
         ref={projectsRef}
         id="projects"
-        className={`w-full px-8 py-16 max-w-6xl mx-auto transform transition-all duration-1000 ${
+        className={`w-full px-8 py-16 max-w-5xl mx-auto transform transition-all duration-1000 ${
           isVisible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"
         }`}
       >
@@ -144,7 +172,7 @@ const App = () => {
               }`}
             >
               {project.imageUrl ? (
-                <div className="w-full aspect-video bg-amber-50 border-2 border-green-800 transition-transform duration-300">
+                <div className="w-full aspect-video bg-amber-50 border-3 border-green-800 transition-transform duration-300">
                   <img
                     src={project.imageUrl}
                     alt={project.title}
@@ -152,15 +180,33 @@ const App = () => {
                   />
                 </div>
               ) : (
-                <div className="w-full aspect-video bg-amber-50 border-2 border-green-800 flex items-center justify-center transition-transform duration-300">
-                  <h3 className="text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-800 to-green-300 inter-300">
+                <div className="w-full aspect-video bg-amber-50 border-3 border-green-800 flex items-center justify-center transition-transform duration-300">
+                  <h3 className="text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-900 to-green-500 inter-300">
                     {project.title}
                   </h3>
                 </div>
               )}
-              <h3 className="text-left text-2xl text-green-950 mt-4">
-                {project.title}
-              </h3>
+              <div className="flex flex-row justify-between items-center w-full">
+                <span className="flex flex-row justify-center items-center">
+                  <h3 className="text-left text-lg font-semibold text-green-950 mt-4 mr-2">
+                    {project.title}
+                  </h3>
+                  <h3 className="text-left text-lg text-green-950 mt-4">|</h3>
+                  <h3 className="text-left text-md text-green-950 mt-4 ml-2">
+                    {project.subtitle}
+                  </h3>
+                </span>
+                <div className="flex flex-wrap gap-2 mt-4 text-lg">
+                  {project.technologies.map((tech, index) => (
+                    <span
+                      key={index}
+                      // className="bg-amber-100 px-2 py-1 rounded-md"
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
             </a>
           ))}
         </div>
