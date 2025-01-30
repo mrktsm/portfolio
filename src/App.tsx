d41b284d85483be446cfbb20209e6ec308e0bf67
Marko Tsymbaluk <markotsymbaluk@gmail.com>
2025-01-30 02:43:09 -0500

    White Space No Wrap

--- src/App.tsx
+++ src/App.tsx
@@ -129,7 +129,7 @@ const App = () => {
       {/* Full-Height Section */}
       <div
         id="About"
-        className="min-h-screen flex flex-col justify-center items-center text-center relative px-3"
+        className="min-h-screen flex flex-col justify-center items-center text-center relative px-3 whitespace-nowrap"
       >
         <div className="flex flex-col items-center mt-6">
           <h1
